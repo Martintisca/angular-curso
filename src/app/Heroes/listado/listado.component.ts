f5c4c91c268df27c3167080b54bd7cdbf83f90cd
martintisca <tisk_16@hotmail.com>
2021-11-04 14:53:25 -0600

    primer commit

---
+++ src/app/Heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent {
+
+  heroes : string[]=['spiderman','ironman',' thor'];
+   heroeBorrado: string='';
+   
+  borrarHeroe(){
+    console.log("borrando");
+      this.heroeBorrado=this.heroes.shift() || '';
+    console.log(this.heroeBorrado);
+    
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contadores/contador/contador.component';
+import { HeroeComponent } from './Heroes/heroe/heroe.component';
+import { ListadoComponent } from './Heroes/listado/listado.component';
+//import { ContadorModule } from './contadores/contador.module';
+import { ContadorModule } from './contadores/contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent,
+    ListadoComponent,
+   
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
